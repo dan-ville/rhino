10376c50c76ea9fe62d618124b8fd2020a399243
Daniel Villegas <daniel.villegas@fsgi.com>
2023-07-01 18:37:35 -0400

    install sass, change app background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import "./globals.css"
 import { Inter } from "next/font/google"
 import { Header } from "@/components"
+import "./globals.css"
+import "../styles/root-layout.scss"
 
 const inter = Inter({ subsets: ["latin"] })
 
